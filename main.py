09d49eb390d83a164e9e1b8c8ec9eedffcbec855
Shashank <shashankdwivedi9648@gmail.com>
2021-09-07 22:54:56 +0530

    Updating

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-from tkinter import Label, mainloop
+from tkinter import *
 class window:
     def __init__(self,title = "DefaultWin"):
         self.name = title
-        Label(text = self.name).pack()
+        self.gui = Tk(className = self.name)
+    def set_size(self,x):
+        self.gui.geometry(x)
     def run(self):
-        mainloop()
+        self.gui.mainloop()
 
-win1 = window()
-win1.run()
-
+if __name__ == "__main__":
+    win1 = window("Main window")
+    win2 = window("2nd Window")
+    win2.set_size("600x600")
+    win1.set_size("500x500")
+    win1.run()
+    win2.run()
